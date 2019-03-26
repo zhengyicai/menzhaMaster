cde201d3712d7e5fea2da8df7b69f9d7f1b38a9a
zhengyicai <1010172623@qq.com>
2019-03-26 15:13:19 +0800

    update

---
+++ qzi-cms-base/qzi-cms-common/src/main/java/com/qzi/cms/common/vo/UseEquipmentNowStateVo.java
@@ -0,0 +1,37 @@
+package com.qzi.cms.common.vo;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2019/3/25.
+ */
+public class UseEquipmentNowStateVo {
+    private String equipmentNo;
+
+    private String state;
+    private String cms;
+
+    public String getEquipmentNo() {
+        return equipmentNo;
+    }
+
+    public void setEquipmentNo(String equipmentNo) {
+        this.equipmentNo = equipmentNo;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCms() {
+        return cms;
+    }
+
+    public void setCms(String cms) {
+        this.cms = cms;
+    }
+}
